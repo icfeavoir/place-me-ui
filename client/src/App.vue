3bd211c6caf2fbce9c6ea44fb6a37898fe1a134f
Pierre Leroy <pierre.leroy.mail@gmail.com>
2020-06-30 09:40:56 +0200

    Popup demo

--- client/src/App.vue
+++ client/src/App.vue
@@ -28,6 +28,27 @@ export default {
   mounted () {
     this.resize()
     this.checkErrors()
+
+    // DEMO : popup pour informer de la liberté !
+    // Local storage => str
+    if (!localStorage.freeToUse || localStorage.freeToUse === 'false') {
+      let msg = {
+        title: 'Place Me',
+        body: 'Sur cette démonstration, vous êtes libre ! Vous pouvez ajouter - modifier - supprimer des réservations, des plans, des évènements !'
+      }
+      let options = {
+        okText: 'C\'est compris !',
+        cancelText: 'Fermer',
+        backdropClose: false
+      }
+      this.$dialog
+        .confirm(msg, options)
+        .then(function (dialog) {})
+        .catch(function () {})
+
+      // Dans tous les cas
+      localStorage.freeToUse = true
+    }
   },
   methods: {
     resize () {
